96e81ce35cd7bf4aa4dda0bbee1408cb0e4d825d
osamamhd <osamamhd82@gmail.com>
2021-08-03 03:24:40 +0200

    ✨ colors, logo, axios.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,49 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="h-screen w-full bg-blue-secondary">
+
+    <div class="mx-auto w-11/12 md:w-4/5 lg:w-1/2 pt-20">
+
+      <div class="w-20 md:w-32 lg:w-40 bg-white-primary p-2 rounded-3xl">
+        <img src="../assets/logo.svg" alt="logo">
+      </div>
+
+      <div class="py-2">
+        <p class="text-3xl md:text-5xl lg:text-7xl text-white-primary font-black">
+          Community for Indie Coder to
+            <span class="text-transparent bg-clip-text bg-gradient-to-br from-pink-400 to-red-600">
+              {{value}}
+            </span>
+          </p>
+      </div>
+
+    </div>
+    
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+  name: 'Home', 
+  data() {
+    return {
+      words: ['meet', 'collaborate', 'share ideas'],
+      value: 'meet',
+      counter: 0,
+      index: 0,
+      interval: 0
+    }
+  },
+  mounted() {
+    this.showWord()
+  },
+  methods: {
+    showWord() {
+      this.interval = setInterval(() => {
+        this.index = ++this.counter % this.words.length;
+        this.value = this.words[this.index]
+      }, 1000)
+    }
+    
   }
 }
 </script>
