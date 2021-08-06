97a839336aefe39e77e65bd61d8ef77beef0f1c5
osamamhd <osamamhd82@gmail.com>
2021-08-07 01:43:06 +0200

    importing axios baseurl.

--- src/main.js
+++ src/main.js
@@ -1,7 +1,14 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import './registerServiceWorker'
+import axios from 'axios';
+import VueAxios from 'vue-axios'
 import router from './router'
 import store from './store'
 import './index.css'
-createApp(App).use(store).use(router).mount('#app')
+
+axios.defaults.baseURL = 'http://127.0.0.1:8000';
+
+createApp(App).use(store, VueAxios, axios).use(router).mount('#app')
+
+
